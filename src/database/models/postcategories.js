e0eb96c698b893470c12f54d7c52532685ddb027
FabioMartins <fabbio1000@gmail.com>
2022-09-01 17:42:11 -0300

    Feat: Criando migration de PostCategories

---
+++ src/database/models/postcategories.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategories = sequelize.define("PostCategories", {
+      postId: {
+        type: DataTypes.INTEGER,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+      },
+    });
+  
+    PostCategories.associate = (models) => {
+      models.BlogPosts.belongsToMany(models.BlogPosts,
+        {
+          as: "post",
+          through: PostCategories,
+          foreignKey: "categoryId",
+          otherKey: "postId",
+        });
+      models.Categories.belongsToMany(models.Categories, {
+        as: "categories",
+        through: PostCategories,
+        foreignKey: "postId",
+        otherKey: "categoryId",
+      });
+    };
+  
+    return PostCategories;
+  };
