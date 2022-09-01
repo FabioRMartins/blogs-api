513f7ccfa3301ac9e04287c41a7b8c8ea09dda3d
FabioMartins <fabbio1000@gmail.com>
2022-09-01 16:23:23 -0300

    Feat: Criando migration para Users

---
+++ src/database/models/user.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define("Users", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return Users;
+};
