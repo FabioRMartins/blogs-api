019f5dee63b73ef0db9a57fe0d0d5ee2e1a1eb9c
FabioMartins <fabbio1000@gmail.com>
2022-09-01 16:40:32 -0300

    Feat: Criando migration para Categories

---
+++ src/database/models/categories.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Categories = sequelize.define("Categories", {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    });
+  
+    return Categories;
+  };
